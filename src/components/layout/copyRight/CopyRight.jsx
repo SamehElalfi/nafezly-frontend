ab3c835e09415e5dee5603e12c657c8875aa0b45
Your Name <you@example.com>
2022-01-31 14:11:17 +0200

    howitworks and card

---
+++ src/components/layout/copyRight/CopyRight.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CopyRight() {
+  return (
+  <div className='flex flex-row justify-around bg-black  text-white'>
+      <span>ar</span>
+      <span>en</span>
+  </div>
+  );
+}
+
+export default CopyRight;
--- src/components/layout/howItWorks/HowItWorks.jsx
+++ src/components/layout/howItWorks/HowItWorks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CheckCircleIcon from '@heroicons/react/solid/CheckCircleIcon'
-
+import payimag from "../../../assets/images/payments-min.png"
 
 
 function Paragraph(props) {
@@ -22,28 +22,28 @@ function Paragraph(props) {
 
 function HowItWorks() {
   return (
-    <div className='flex flex-row container'>
+    <div className='flex flex-row container justify-around'>
       <div className='rightSide h-100 w-1/2' >
         < Paragraph
           title=' 1اضف مشروع'
-          paragraph='هذا هو العنواهذا هو العنواهذا هو العنواهذا هو هذا هو العنواهذا هو العنوا الاول لهذة المدونة النجسة'
+          paragraph='هذا هو العنواهذا هو العنواهذا هو العنواهذا هو هذا هو العنواهذا هو العنوا الاول لهذة المدونة '
         />
         < Paragraph
           title=' 2اضف مشروع'
-          paragraph='هذا هو اهذا هو العنواهذا هو العنواهذا هو هذا هو العنواهذا هو العنوا الاول لهذة المدونة النجسة'
+          paragraph='هذا هو اهذا هو العنواهذا هو العنواهذا هو هذا هو العنواهذا هو العنوا الاول لهذة المدونة '
         />
         < Paragraph
           title=' 3اضف مشروع'
-          paragraph='هذا هو الهذا هو العنواهذا هو العنواهذا هو العنواهذا هو العنواعنوان الاول لهذة المدونة النجسة'
+          paragraph='هذا هو الهذا هو العنواهذا هو العنواهذا هو العنواهذا هو العنواعنوان الاول لهذة المدونة '
         />
       </div>
       <div className='leftSide mt-5 h-100 w-1/2'>
 
-          <iframe
+          <img
             className='w-full h-full '
-            title='newone'
-            src="https://www.youtube.com/embed/UBOj6rqRUME"
-          ></iframe>
+            alt='newone'
+            src={payimag}
+          />
         </div>
       </div>
   );
