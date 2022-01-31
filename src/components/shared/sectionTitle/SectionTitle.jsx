fd1b3003a78066e74e5a81ee882bb49b3b86d853
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-01-31 17:47:44 +0200

    fix: padding on small screens

--- src/components/shared/sectionTitle/SectionTitle.jsx
+++ src/components/shared/sectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function SectionTitle({children}) {
-  return <div className='text-2xl text-center my-24 select-none'> { children } </div>;
+  return <div className='text-2xl text-center my-12 md:my-24 select-none'> { children } </div>;
 }
 
 export default SectionTitle;
