35f741e0a8ab47ac12259fdfefb0472b778f8929
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-01-31 17:45:04 +0200

    fix: add padding to buttons

--- src/components/shared/iconButton/IconButton.jsx
+++ src/components/shared/iconButton/IconButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function IconButton(props) {
     return (
        <a href={props.link || "#"}>
-            <div className={`flex flex-row gap-2 m-2 items-center hover:bg-gray-200 ${props.divStyle}`}>
+            <div className={`flex flex-row gap-2 m-2 p-2 items-center hover:bg-gray-200 ${props.divStyle}`}>
                 <div>
                     <props.icon className={`text-gray-600 ${props.classNameIcon}`} />
                 </div>
