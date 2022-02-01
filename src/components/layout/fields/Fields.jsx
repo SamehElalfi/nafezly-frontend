c2852e60b3fb2e1bcd223913ce13eb1cec765f84
Your Name <you@example.com>
2022-02-01 13:46:25 +0200

    feat: added filds section

---
+++ src/components/layout/fields/Fields.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Card from "../../shared/card/Card";
+import { FaPencilAlt } from "react-icons/fa";
+
+function Fields() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 p-12">
+      <Card
+        icon={FaPencilAlt}
+        name="تصميم و أعمال"
+        title="التصميم  الجرافيك "
+        text="التصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيك"
+      />
+
+      <Card
+        icon={FaPencilAlt}
+        name="تصميم و أعمال"
+        title="التصميم  الجرافيك "
+        text="التصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيك"
+      />
+
+      <Card
+        icon={FaPencilAlt}
+        name="تصميم و أعمال"
+        title="التصميم  الجرافيك "
+        text="التصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيك"
+      />
+
+      <Card
+        icon={FaPencilAlt}
+        name="تصميم و أعمال"
+        title="التصميم  الجرافيك "
+        text="التصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيك"
+      />
+
+      <Card
+        icon={FaPencilAlt}
+        name="تصميم و أعمال"
+        title="التصميم  الجرافيك "
+        text="التصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيك"
+      />
+
+      <Card
+        icon={FaPencilAlt}
+        name="تصميم و أعمال"
+        title="التصميم  الجرافيك "
+        text="التصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيكالتصميم  الجرافيك"
+      />
+    </div>
+  );
+}
+
+export default Fields;
