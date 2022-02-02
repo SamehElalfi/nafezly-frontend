989d908c0fdda4803fb70e9761db791de9d89a70
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-02-02 17:32:51 +0200

    use nouislider-react component

--- src/components/layout/filters/Filters.jsx
+++ src/components/layout/filters/Filters.jsx
@@ -1,86 +1,114 @@
-import React from "react";
-import {FaUserTie} from "react-icons/fa"
-import {BiCodeAlt} from "react-icons/bi"
-import {FaPaintBrush} from "react-icons/fa"
-import {BsBullseye} from "react-icons/bs"
-import {FaPenAlt} from "react-icons/fa"
-import {BsHeadset} from "react-icons/bs"
-import {IoIosArrowForward} from "react-icons/io"
-import {AiOutlineEllipsis} from "react-icons/ai"
-import MultiRangeSlider from "components/shared/muiltRangeSlider/MultiRangeSlider";
+import React, { useState } from "react";
+
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
+
+import Input from "components/shared/input/Input";
+
+// icons
+import { AiOutlineEllipsis } from "react-icons/ai";
+import { FaUserTie, FaPenAlt, FaPaintBrush } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { BiCodeAlt } from "react-icons/bi";
+import { BsBullseye, BsHeadset } from "react-icons/bs";
 
 function Filters({ className }) {
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(100);
+
+  const onSlide = (render, handle, value, un, percent) => { 
+    setMinPrice(parseInt(value[0]))
+    setMaxPrice(parseInt(value[1]))
+  }
+
   return (
     <div
-      className={` flex w-1/4 flex-col gap-3 h-[700px] bg-red-400 p-5   ${
+      className={`flex w-full flex-col gap-3 rounded-sm px-5 py-2 pb-16 ${
         className || ""
       } `}
     >
-        <div className="sm:hidden">
-            <div className="flex flex-row justify-between ">
-                <div className='flex flex-row items-center '>
-                    <IoIosArrowForward/>
-                    <span className="text-lg">عودة</span>
-                </div>
-                <span className="text-lg">نتائج البحث</span>
+      <div className="sm:hidden">
+        <div className="flex flex-row justify-between ">
+          <div className="flex flex-row items-center ">
+            <IoIosArrowForward />
+            <span className="text-lg">عودة</span>
+          </div>
+          <span className="text-lg">نتائج البحث</span>
         </div>
-        </div>
-      <span className=" text-lg mt-3">بحث</span>
+      </div>
+      <Title title="بحث" />
       <div className="">
-        <input type="text" className="outline-none h-8 w-full" />
+        <Input name="query" label=" " />
       </div>
-      <span className="text-lg">التصنيفات</span>
+      <Title title="التصنيفات" />
       <div className="flex flex-col gap-2 ">
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="management" id="management" />
-          <FaUserTie className=""w-4/>
-          <label htmlFor="management">أعمال وخدمات استشارية وإدارية</label>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="programming" id="programming" />
-          <BiCodeAlt className=""w-4/>
-          <label htmlFor="programming"> برمجة، تطوير المواقع والتطبيقات</label>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="design" id="design" />
-          <FaPaintBrush className=""w-4/>
-          <label htmlFor="design"> تصميم وأعمال فنية وإبداعية</label>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="marketing" id="marketing" />
-          <BsBullseye className=""w-4/>
-          <label htmlFor="marketing">تسويق الكتروني ومبيعات</label>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="writing" id="writing" />
-          <FaPenAlt className=""w-4/>
-          <label htmlFor="writing">كتابة، صناعة محتوى، ترجمة ولغات</label>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="training" id="training" />
-          <BsHeadset className=""w-4/>
-          <label htmlFor="training"> تدريب، تعليم ومساعدة عن بعد</label>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          <input className="h-[30px] w-[20px]" type="checkbox" name="others" id="others" />
-          <AiOutlineEllipsis className=""w-4/>
-          <label htmlFor="others"> أمور أخرى</label>
-        </div>
-      
-        <span className=" text-lg mb-2 mt-3">المهارات</span>
+        <CheckboxFilter
+          title="أعمال وخدمات استشارية وإدارية"
+          icon={FaUserTie}
+          name="works"
+        />
+        <CheckboxFilter
+          title="برمجة، تطوير المواقع والتطبيقات"
+          icon={BiCodeAlt}
+          name="programming"
+        />
+        <CheckboxFilter
+          title="تصميم وأعمال فنية وإبداعية"
+          icon={FaPaintBrush}
+          name="design"
+        />
+        <CheckboxFilter
+          title="تسويق الكتروني ومبيعات"
+          icon={BsBullseye}
+          name="marketing"
+        />
+        <CheckboxFilter
+          title="كتابة، صناعة محتوى، ترجمة ولغات"
+          icon={FaPenAlt}
+          name="writing"
+        />
+        <CheckboxFilter
+          title="تدريب، تعليم ومساعدة عن بعد"
+          icon={BsHeadset}
+          name="training"
+        />
+        <CheckboxFilter
+          title="أمور أخرى"
+          icon={AiOutlineEllipsis}
+          name="other"
+        />
+
+        <Title title="المهارات" />
+          <Input name="skills" label=" " />
         <div className="">
-            <input type="text" className="outline-none h-8 w-full" />
         </div>
-      
-        <span className=" text-lg mt-3">الميزانية</span>
-        <div className="py-3">
-            <MultiRangeSlider
-                min={10}
-                max={10000}
-                onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
-            />
+
+        <Title title="الميزانية" />
+        <Nouislider start={[10, 100]} range={{ 'min': 0, 'max': 100 }} onSlide={onSlide} />
+        <div className="flex flex-row-reverse justify-between">
+          <span>{ minPrice }</span>
+          <span>{ maxPrice }</span>
         </div>
       </div>
+    </div>
+  );
+}
+
+function Title({ title }) {
+  return <span className="mt-3 text-lg">{title}</span>;
+}
+
+function CheckboxFilter(props) {
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <input
+        className="h-[30px] w-[20px]"
+        type="checkbox"
+        id={props?.name}
+        name="filter"
+      />
+      <props.icon className="w-4" />
+      <label htmlFor={props?.name}>{props?.title}</label>
     </div>
   );
 }
