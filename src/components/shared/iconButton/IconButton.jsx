ae86d65016ad6a17d3138cb8e7f42f0033bd2cea
fahmy99 <davidhenry.mine@gmail.com>
2022-02-10 15:05:14 +0200

    fix:style:iconButton

--- src/components/shared/iconButton/IconButton.jsx
+++ src/components/shared/iconButton/IconButton.jsx
@@ -4,15 +4,23 @@ function IconButton(props) {
   return (
     <a href={props.link || "#"} onClick={props.onClick && props.onClick}>
       <div
-        className={`m-2 flex flex-row items-center gap-2 p-2 hover:bg-blue-300 bg-yellow-300  ${props.divStyle}`}
+        className={`m-2 flex flex-row items-center gap-2 p-2 hover:bg-blue-300  ${props.divStyle || ""}`}
       >
         <div>
-          <props.icon className={`text-black w-4 ${props.classNameIcon}`} />
+          <props.icon className={`text-black w-4 ${props.classNameIcon || ""}`} />
         </div>
         <span> {props.name} </span>
       </div>
     </a>
   );
 }
+
+
+  //  props
+//  onclick: optional
+//  divStyle: optional
+//  name: optional
+//  icon: required
+//  classNameIcon: optional
 
 export default IconButton;
