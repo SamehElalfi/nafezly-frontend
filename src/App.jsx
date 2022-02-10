87edb14934cd61e92a7ec6de3c6f6a6a76c58d00
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-02-10 17:57:54 +0200

    feat: add 404 path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "components/layout/footer/Footer";
 import Navbar from "components/layout/navbar/Navbar";
 import HomePage from "pages/HomePage/HomePage";
 import LoginPage from "pages/LoginPage/LoginPage";
+import PasswordResetPage from "pages/PasswordResetPage/PasswordResetPage";
 import ProjectPage from "pages/ProjectPage/ProjectPage";
 import ProjectsPage from "pages/ProjectsPage/ProjectsPage";
 import RegistrationPage from "pages/RegistrationPage/RegistrationPage";
@@ -18,8 +19,14 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/project/:projectId" element={<ProjectPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
+
+          <Route>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            <Route path="/password-reset" element={<PasswordResetPage />} />
+          </Route>
+          
+          <Route path="*" element={<h1 className="mt-96 text-2xl">not found 404</h1>}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
